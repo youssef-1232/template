43f6e1939e92f4d578a32f3ef6a12ef6313df136
youssef_selmy <ym79793@gmail.com>
2023-06-22 22:00:57 +0300

    template

---
+++ main.js
@@ -0,0 +1,21 @@
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+
+// console.log(tabsArray);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    // console.log(ele);
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    // console.log(e.currentTarget.dataset.cont);
+    document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+  });
+});
